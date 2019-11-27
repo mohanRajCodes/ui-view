a73cad790a3aa9a3525fc9586a54983e08d5f10d
mohanRajCodes <56564336+mohanRajCodes@users.noreply.github.com>
2019-11-27 17:35:55 +0530

    Updating dataService

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.get('/',function(req,res){
   res.sendFile(__dirname + '/public/view/Home.html');
 });
 
+
+app.get('/employee/:empno',function(req,res){
+
+    var empnbr = req.params.empno;
+
+    var employeeDetail = [{empno:1,name:"Kohli",gender:"Male",Age:"31",Role:"Captain"},
+                  {empno:2,name:"Rohit sharma",gender:"Male",Age:"32",Role:"Vice Captain"}];
+    
+    
+    res.send(employeeDetail.filter((emp)=>emp.empno == empnbr));
+    
+});
+
 app.listen(2900,function(){
 
   console.log("server is running in port 2900");
